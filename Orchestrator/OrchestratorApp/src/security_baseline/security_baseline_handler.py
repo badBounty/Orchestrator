c8e9c3c97a76092579fcb5e2ff8e058830896d5e
Handerllon <handerllon@Handerllon.Handerllon>
2020-04-30 08:48:39 -0400

    update

--- Orchestrator/OrchestratorApp/src/security_baseline/security_baseline_handler.py
+++ Orchestrator/OrchestratorApp/src/security_baseline/security_baseline_handler.py
@@ -3,13 +3,13 @@ from ...tasks import header_scan_task, http_method_scan_task, ssl_tls_scan_task
 
 def handle_target_baseline_security_scan(target_name, language):
     header_scan_task.delay(target_name, 'TARGET', language)
-    #http_method_scan_task.delay(target_name, 'TARGET', language)
-    #ssl_tls_scan_task.delay(target_name, 'TARGET', language)
+    http_method_scan_task.delay(target_name, 'TARGET', language)
+    ssl_tls_scan_task.delay(target_name, 'TARGET', language)
     return
 
 
 def handle_url_baseline_security_scan(single_url, language):
     header_scan_task.delay(single_url, 'SINGLE', language)
-    #http_method_scan_task.delay(single_url, 'SINGLE', language)
-    #ssl_tls_scan_task.delay(single_url, 'SINGLE', language)
+    http_method_scan_task.delay(single_url, 'SINGLE', language)
+    ssl_tls_scan_task.delay(single_url, 'SINGLE', language)
     return
