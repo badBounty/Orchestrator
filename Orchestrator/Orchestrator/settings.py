2cc98003a6336321be93199a94cf4a092fac080e
handerllon <manuel121097@gmail.com>
2020-05-05 11:28:11 -0400

    Update

--- Orchestrator/Orchestrator/settings.py
+++ Orchestrator/Orchestrator/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '159.203.100.107']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '167.71.106.245']
 
 
 # Application definition
