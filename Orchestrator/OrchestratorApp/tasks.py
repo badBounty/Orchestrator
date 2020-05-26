3fc9b4a3e71997ab079e73eb51442f47c4dc58d9
handerllon <manuel121097@gmail.com>
2020-05-26 15:30:04 +0000

    Commented some tasks for testing

--- Orchestrator/OrchestratorApp/tasks.py
+++ Orchestrator/OrchestratorApp/tasks.py
@@ -27,8 +27,8 @@ def sleepy(duration):
 def vuln_scan_with_email_notification(email, url_to_scan, language, report_type, redmine_project):
     vuln_scan_single_task(url_to_scan, language, redmine_project)
     vulns = mongo.get_vulns_with_language(url_to_scan, language)
-    file_dir, missing_findings = reporting.create_report("", language, report_type, url_to_scan, vulns)
-    email_handler.send_email(file_dir, missing_findings, email)
+    #file_dir, missing_findings = reporting.create_report("", language, report_type, url_to_scan, vulns)
+    #email_handler.send_email(file_dir, missing_findings, email)
 
 
 # ------------------ Full tasks ------------------ #
