969a11b9e326cd68d45bfb66b9bc82e022209752
ferT <ferd.tavella@gmail.com>
2020-05-29 13:24:11 -0400

    for merge

--- Orchestrator/OrchestratorApp/tasks.py
+++ Orchestrator/OrchestratorApp/tasks.py
@@ -25,14 +25,14 @@ def sleepy(duration):
 # ------------------ Scan with email ------------------ #
 @shared_task
 def vuln_scan_with_email_notification(info):
-    vuln_scan_single_task(info)
+    #vuln_scan_single_task(info)
     vulns = mongo.get_vulns_with_language(info['target'], info['selected_language'])
-    #file_dir, missing_findings = reporting.create_report("", info, vulns)
-    #email_handler.send_email(file_dir, missing_findings, info['email'])
-    #try:
-    #    os.remove(file_dir)
-    #except FileNotFoundError:
-    #    pass
+    file_dir, missing_findings = reporting.create_report("", info, vulns)
+    email_handler.send_email(file_dir, missing_findings, info['email'])
+    try:
+        os.remove(file_dir)
+    except FileNotFoundError:
+        pass
     gc.collect()
 
 
@@ -112,8 +112,8 @@ def vuln_scan_single_task(info):
     header_scan.handle_single(scan_information)
     http_method_scan.handle_single(scan_information)
     cors_scan.handle_single(scan_information)
-    libraries_scan.handle_single(scan_information)
-    #ssl_tls_scan.handle_single(scan_information)
+    #libraries_scan.handle_single(scan_information)
+    ssl_tls_scan.handle_single(scan_information)
     # Extra
     ffuf.handle_single(scan_information)
     # Nmap scripts
