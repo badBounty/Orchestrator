d36ff10be932b736b30c4f10c202f6ee1a184b3e
handerllon <manuel121097@gmail.com>
2020-06-03 15:15:10 -0300

    Big update

--- Orchestrator/OrchestratorApp/src/security/vuln_scan_handler.py
+++ Orchestrator/OrchestratorApp/src/security/vuln_scan_handler.py
@@ -1,15 +1,68 @@
-from ...tasks import vuln_scan_single_task, vuln_scan_target_task, vuln_scan_with_email_notification, recon_and_vuln_scan_task, vuln_scan_file_input_task
+from ...tasks import recon_and_vuln_scan_task
+from ...tasks import header_scan_task, http_method_scan_task, cors_scan_task, libraries_scan_task, ssl_tls_scan_task, ffuf_task, nmap_script_scan_task, iis_shortname_scan_task, bucket_finder_task, token_scan_task, css_scan_task, firebase_scan_task, host_header_attack_scan, burp_scan_task
+from ..mongo import mongo
 import os
 
 
+def handle_file(info, f):
+    url_list = list()
+    for line in f.readlines():
+        url_list.append(line.decode().replace('\r\n',''))
+
+    if not info['checkbox_redmine']:
+        info['redmine_project'] = 'no_project'
+
+    scan_information = {
+        'target': str(f),
+        'url_to_scan': None,
+        'language': info['selected_language'],
+        'redmine_project': info['redmine_project'],
+        'invasive_scans': info['use_active_modules'],
+        'assigned_users': info['assigned_users'],
+        'watchers': info['watcher_users']
+    }
+
+    scan_information['url_to_scan'] = url_list
+    handle_multiple_scan(scan_information)
+
+    return
+
 def handle(info):
-
     # Here we parse the information and call each scan type
-    print(info)
+    # We are only going to parse scan information for now, the rest will prob be at settings
+    if not info['checkbox_redmine']:
+        info['redmine_project'] = 'no_project'
 
+    if info['scan_type'] == 'existing_target':
+        handle_target_scan(info)
+    #TODO
+    elif info['scan_type'] == 'new_target':
+        pass
+        #handle_new_target_scan(info)
+    elif info['scan_type'] == 'single_target':
+        handle_single_scan(info)
+
+    return
 
 def handle_target_scan(info):
-    vuln_scan_target_task.delay(info)
+    scan_information = {
+        'target': info['existing_target_choice'],
+        'url_to_scan': info['existing_target_choice'],
+        'language': info['selected_language'],
+        'redmine_project': info['redmine_project'], 
+        'invasive_scans': info['use_active_modules'],
+        'assigned_users': info['assigned_users'],
+        'watchers': info['watcher_users']
+    }
+    subdomains_http = mongo.get_responsive_http_resources(scan_information['target'])
+    only_urls = list()
+    for subdomain in subdomains_http:
+        only_urls.append(subdomain['url_with_http'])
+    scan_information['url_to_scan'] = only_urls
+
+    # Run the scan
+    handle_multiple_scan(scan_information)
+
     return
 
 
@@ -18,54 +71,54 @@ def handle_new_target_scan(info):
     return
 
 
-def handle_file_target_scan(info, file):
-    url_list = list()
-    for line in f.readlines():
-        url_list.append(line.replace('\n',''))
+def handle_multiple_scan(info):
 
-    info_to_send = {'target': 'file_target', 'selected_language': 'eng', 'use_active_modules': True}
+    header_scan_task.apply_async(args=['target', info], queue='fast_queue')
+    http_method_scan_task.apply_async(args=['target', info], queue='fast_queue')
+    libraries_scan_task.apply_async(args=['target', info], queue='fast_queue')
+    ffuf_task.apply_async(args=['target', info], queue='fast_queue')
+    iis_shortname_scan_task.apply_async(args=['target', info], queue='fast_queue')
+    bucket_finder_task.apply_async(args=['target', info], queue='fast_queue')
+    token_scan_task.apply_async(args=['target', info], queue='fast_queue')
+    css_scan_task.apply_async(args=['target', info], queue='fast_queue')
+    firebase_scan_task.apply_async(args=['target', info], queue='fast_queue')
+    host_header_attack_scan.apply_async(args=['target', info], queue='fast_queue')
 
-    vuln_scan_file_input_task.delay(info_to_send, url_list)
+    # Slow queue
+    cors_scan_task.apply_async(args=['target', info], queue='slow_queue')
+    ssl_tls_scan_task.apply_async(args=['target', info], queue='slow_queue')
+    nmap_script_scan_task.apply_async(args=['target', info], queue='slow_queue')
+    burp_scan_task.apply_async(args=['target', info], queue='slow_queue')    
     return
+    
 
-
-def handle_url_baseline_security_scan(single_url, language):
-    vuln_scan_single_task.delay(single_url, language)
-    return
-
-
-def handle_scan_with_email_notification(info):
-    vuln_scan_with_email_notification.delay(info)
-    return
-
-
-def single_scan_handler(info):
-    from ...tasks import header_scan_task, http_method_scan_task, cors_scan_task, libraries_scan_task, ssl_tls_scan_task, ffuf_task, nmap_script_scan_task, iis_shortname_scan_task, bucket_finder_task, token_scan_task, css_scan_task, firebase_scan_task, host_header_attack_scan, burp_scan_task
+def handle_single_scan(info):
     scan_information = {
-        'target': info['target'],
-        'url_to_scan': info['target'],
+        'target': info['single_target_choice'],
+        'url_to_scan': info['single_target_choice'],
         'language': info['selected_language'],
         'redmine_project': info['redmine_project'],
-        'invasive_scans': info['use_active_modules'],
+        #TODO 
+        'invasive_scans': False,
         'assigned_users': info['assigned_users'],
         'watchers': info['watcher_users']
     }
 
     # Fast queue
-    header_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')         ###
-    http_method_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')    ###
-    libraries_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')      ###
-    ffuf_task.apply_async(args=['single', scan_information], queue='fast_queue')                ###
-    iis_shortname_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')  ###
-    bucket_finder_task.apply_async(args=['single', scan_information], queue='fast_queue')       ###
-    token_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')         ###
-    css_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')           ###
-    firebase_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')       ###
-    host_header_attack_scan.apply_async(args=['single', scan_information], queue='fast_queue') ###
+    header_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')
+    http_method_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')
+    libraries_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')
+    ffuf_task.apply_async(args=['single', scan_information], queue='fast_queue')
+    iis_shortname_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')
+    bucket_finder_task.apply_async(args=['single', scan_information], queue='fast_queue')
+    token_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')
+    css_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')
+    firebase_scan_task.apply_async(args=['single', scan_information], queue='fast_queue')
+    host_header_attack_scan.apply_async(args=['single', scan_information], queue='fast_queue')
 
     # Slow queue
-    cors_scan_task.apply_async(args=['single', scan_information], queue='slow_queue')      ###
-    ssl_tls_scan_task.apply_async(args=['single', scan_information], queue='slow_queue')   ###
+    cors_scan_task.apply_async(args=['single', scan_information], queue='slow_queue')
+    ssl_tls_scan_task.apply_async(args=['single', scan_information], queue='slow_queue')
     nmap_script_scan_task.apply_async(args=['single', scan_information], queue='slow_queue')
     burp_scan_task.apply_async(args=['single', scan_information], queue='slow_queue')
     return
--- Orchestrator/OrchestratorApp/urls.py
+++ Orchestrator/OrchestratorApp/urls.py
@@ -6,12 +6,10 @@ app_name = 'OrchestratorApp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('recon/', views.recon_view, name='recon'),
-    path('target_scan/', views.target_scan_view, name='target_scan'),
-    path('scan_with_email/', views.email_scan_view, name='scan_with_email'),
     path('workspaces/', views.show_workspaces, name='workspaces'),
     path('workspaces/<str:target_name>', views.show_workspace, name='workspace'),
     path('vulnerabilities', views.show_vulns, name='vulnerabilities'),
     path('vulnerabilities/<str:target_name>', views.show_project_vulns, name='project_vulns'),
     path('slack_in', views.slack_input, name='slack_in'),
-    path('test_area/', views.test_view, name='test_area')
+    path('vulnerability_scan/', views.vuln_scan_view, name='vulnerability_scan')
 ]
